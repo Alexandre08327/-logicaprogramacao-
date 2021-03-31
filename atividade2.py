4a103635495b540de0da8dcee7ce1e3c71a7f911
Alexandre08327 <81171361+Alexandre08327@users.noreply.github.com>
2021-03-31 15:31:44 -0300

    Create atividade2.py

---
+++ atividade2.py
@@ -0,0 +1,46 @@
+def calcula_soma(lista):
+  lista = []
+  soma = 0
+  for num in lista:
+    soma += num
+  return soma
+
+retorno = calcula_soma([])
+print(retorno)
+
+______________________________________________________
+
+def converte_entrada(texto):
+  texto = str(input())
+  lista = texto.split()
+  return lista
+
+retorno = converte_entrada('texto')  
+print(retorno)
+
+______________________________________________________
+
+def processa_numeros(lista):
+  lista = []
+  soma = 0
+  elementos = len(lista)
+  for num in lista:
+    soma += num 
+  return (soma, elementos)
+  
+retorno = processa_numeros([])
+print(retorno)
+
+_______________________________________________________
+
+def main():
+  entrada = input() 
+  lista = entrada.split()
+  soma = 0 
+  for num in lista:
+    soma += int(num)
+  media = soma/len(lista)
+  return media
+
+retorno = main()
+print(retorno)
